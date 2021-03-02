0f06335659a45122f5c12fc8559eb9a5a31646d0
micaelacolpi <micaelacolpi@yahoo.com>
2021-03-02 13:39:12 -0600

    added img file and photos, and added data to js

---
+++ scripts/australia/landmarks/landmarkData.js
@@ -0,0 +1,23 @@
+
+LANDMARK DATA 
+const landmarkCollection = [ 
+{ 
+    landmarkName: "Sydney Opera House", 
+    landmarkImage: "Portugal-LandmarkPena", 
+    landmarkLocation: "Sydney, Australia", 
+    openWithCovid: true 
+}, 
+{
+     landmarkName: "Port Arthur", 
+     landmarkImage: "idk",
+      landmarkLocation: "Town in Tasmania, Australia", 
+      openWithCovid: true 
+    },
+    {
+         landmarkName: "Uluru", 
+         landmarkImage: "idk", 
+         landmarkLocation: "", 
+         openWithCovid: false 
+        }
+     ]
+
---
+++ scripts/australia/people/peopleData.js
@@ -0,0 +1,22 @@
+PERSON DATA 
+const peopleCollection = [ 
+    {
+        personName: "Nicole Kidman", 
+        personImage: "i dont have ", 
+         famousFor: "Acting", 
+         aliveToday: true 
+        }, 
+        { 
+            personName: "Chris Hemsworth", 
+            personImage: "Portugal-PeopleMagellan", 
+            famousFor: " Acting", 
+            aliveToday: true 
+        }, 
+        { 
+            personName: "Heath Ledger", 
+            personImage: "Portugal-PeopleFortado", 
+            famousFor: "Acting", 
+            aliveToday: false 
+        } 
+    ]
+
