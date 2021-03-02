91cefca43338c6ef94c9cb1c3730916c0a52ee94
Laurel Morrison <lbentleym1992@gmail.com>
2021-03-02 16:43:46 -0500

    updating css for cards

--- scripts/portugal/landmarks/landmark.js
+++ scripts/portugal/landmarks/landmark.js
@@ -1,7 +1,7 @@
 export const landmark = (landmarkObj) => {
     return `
       <article class="landmark_card">
-          <div><img class="city_card_image" src="images/portugal_img/${landmarkObj.landmarkImage}" alt="Portugal landmark Image"></div>
+          <div><img class="landmark_card_image" src="images/portugal_img/${landmarkObj.landmarkImage}" alt="Portugal landmark Image"></div>
           <div class="landmark_name"><h3>${landmarkObj.landmarkName}</h3></div>
           <div class="landmark_location">Location: ${landmarkObj.landmarkLocation}</div>
           <div class="landmark_covid">Open during Pandemic: ${landmarkObj.openWithCovid === true ? "Yes" : "No"}</div>
