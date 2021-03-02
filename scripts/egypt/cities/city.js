7963b881e08dafd3ac212e6d05692a57dc8523e0
srcrank <salmacrank@gmail.com>
2021-03-02 12:02:17 -0600

    js on egypt city

---
+++ scripts/egypt/cities/city.js
@@ -0,0 +1,10 @@
+export const city = (cityObj) => {
+  return `
+    <article class="city_card">
+        <div><img class="city_card_image" src="images/egypt_img/${cityObj.cityImage}" alt="Egypt City Image"></div>
+        <div class="city_name"><h3>${cityObj.cityName}</h3></div>
+        <div class="city_population">${cityObj.cityPopulation}</div>
+        <div class="city_fact">${cityObj.cityFact}</div>
+    </article>
+    `;
+};
