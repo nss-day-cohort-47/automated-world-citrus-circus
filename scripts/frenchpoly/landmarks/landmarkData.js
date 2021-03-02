e185c828901e0f34d8aabb7c5fd42b1a5f0e537d
Coachbre <brekonan1@gmail.com>
2021-03-02 11:13:43 -0500

    edited all js files

---
+++ scripts/frenchpoly/landmarks/landmarkData.js
@@ -0,0 +1,25 @@
+
+const landmarkCollection = [
+    {
+        landmarkName: "Eiao Cliffs",
+        landmarkLocation: "Sintra",
+        openWithCovid: true
+        landmarkImage: "Portugal-LandmarkPena",
+    },
+    {
+        landmarkName: "Palace of Pena",
+        landmarkLocation: "Sintra",
+        openWithCovid: true
+        landmarkImage: "Portugal-LandmarkPena",
+    },
+    {
+        landmarkName: "Palace of Pena",
+        landmarkLocation: "Sintra",
+        openWithCovid: true
+        landmarkImage: "Portugal-LandmarkPena",
+    }
+]
+
+
+
+
